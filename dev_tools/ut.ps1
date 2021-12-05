654227e8bb624554629fef1adcdc4e40192e2cd3
unknown <maerz.clemens@yahoo.com>
2021-12-06 00:15:58 +0100

    Development tools unter dev-tool eingefügt. Mögliches display getestet

---
+++ dev_tools/ut.ps1
@@ -0,0 +1,19 @@
+
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$fn 
+)
+
+[string] $loc = Get-Location 
+
+
+[string] $f = $loc + "\" + $fn
+
+scp $f pi@192.168.0.10:software
+
+$c = "sudo python3 software/" + $fn 
+
+ssh 192.168.0.10 -p 22 -l pi -t $c 
+
+
+
